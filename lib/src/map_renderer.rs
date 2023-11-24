9c1acbe945952228d04f86f31d2b42d05360959b
Nithanim <git@nithanim.me>
2023-11-24 21:28:59 +0100

    Refactoring

--- lib/src/map_renderer.rs
+++ lib/src/map_renderer.rs
@@ -8,15 +8,6 @@ use crate::api::matchup::Matchup;
 use crate::api::objective::Objective;
 use crate::api::owner::OwningForce;
 
-fn get_owning_force_tint(o: &OwningForce) -> ImVec4 {
-    let power = 0.3;
-    match o {
-        OwningForce::RED => ImVec4::new(1.0, power, power, 1.0),
-        OwningForce::GREEN => ImVec4::new(power, 1.0, power, 1.0),
-        OwningForce::BLUE => ImVec4::new(power, power, 1.0, 1.0),
-        OwningForce::NEUTRAL => ImVec4::new(1.0, 1.0, 1.0, 1.0),
-    }
-}
 
 pub unsafe fn render_map(objectives: &Vec<ObjectiveDefinition>, icons: &HashMap<icons::Icon, ImGuiIcon>, matchup: &Matchup) {
     const MAP: &str = "Center";
@@ -53,36 +44,12 @@ pub unsafe fn render_map_(objective_definitions: &Vec<&ObjectiveDefinition>, ico
     let uv1 = ImVec2::new(1.0, 1.0);
     let border_color = ImVec4::new(0.0, 0.0, 0.0, 0.0);
 
-    let min_x = objective_definitions.iter()
-        .map(|c| c.coord)
-        .filter(|c| c.is_some())
-        .map(|c| c.unwrap()[0])
-        .reduce(f32::min).unwrap_or(0.0);
-    let max_x = objective_definitions.iter()
-        .map(|c| c.coord)
-        .filter(|c| c.is_some())
-        .map(|c| c.unwrap()[0])
-        .reduce(f32::max).unwrap_or(0.0);
-    let min_y = objective_definitions.iter()
-        .map(|c| c.coord)
-        .filter(|c| c.is_some())
-        .map(|c| c.unwrap()[1])
-        .reduce(f32::min).unwrap_or(0.0);
-    let max_y = objective_definitions.iter()
-        .map(|c| c.coord)
-        .filter(|c| c.is_some())
-        .map(|c| c.unwrap()[1])
-        .reduce(f32::max).unwrap_or(0.0);
+    let map_dimensions = calc_map_dimensions(objective_definitions);
 
     let icon_size = ImVec2::new(32f32, 32f32);
-    let map_size_x = max_x - min_x;
-    let map_size_y = max_y - min_y;
 
     for objective_def in objective_definitions {
         let objective_live = objectives.get(&objective_def.id);
-        let tint = get_owning_force_tint(&objective_live
-            .map(|e| &e.owner)
-            .unwrap_or(&OwningForce::NEUTRAL));
 
         let objective_icon = match &objective_def.type_ {
             objective_definition::Type::CAMP => Some(&icons::Icon::ObjectiveCamp),
@@ -92,15 +59,16 @@ pub unsafe fn render_map_(objective_definitions: &Vec<&ObjectiveDefinition>, ico
             _default => None,
         }.map(|m| icons.get(m).unwrap());
 
-        let x = (objective_def.coord.unwrap()[0] - min_x) / map_size_x * (available_area.x - icon_size.x);
-        let y = (objective_def.coord.unwrap()[1] - min_y) / map_size_y * (available_area.y - icon_size.x);
+        let x = (objective_def.coord.unwrap()[0] - map_dimensions.min_x) / map_dimensions.w * (available_area.x - icon_size.x);
+        let y = (objective_def.coord.unwrap()[1] - map_dimensions.min_y) / map_dimensions.h * (available_area.y - icon_size.x);
 
         igSetCursorPos(ImVec2::new(pos.x + x, pos.y + y));
 
         if objective_icon.is_some() {
-            let x1 = objective_icon.unwrap();
+            let tint = get_owning_force_tint_objective(&objective_live);
+
             igImage(
-                x1.to_imgui_id(),
+                objective_icon.unwrap().to_imgui_id(),
                 objective_icon.unwrap().size, uv0,
                 uv1,
                 tint,
@@ -115,4 +83,66 @@ pub unsafe fn render_map_(objective_definitions: &Vec<&ObjectiveDefinition>, ico
 
     //igDummy(ImVec2::new(available_area.x, available_area.y));
     igEnd();
+}
+
+unsafe fn get_owning_force_tint_objective(objective_live: &Option<&&Objective>) -> ImVec4 {
+    get_owning_force_tint_force(&objective_live
+        .map(|e| &e.owner)
+        .unwrap_or(&OwningForce::NEUTRAL))
+}
+
+fn get_owning_force_tint_force(o: &OwningForce) -> ImVec4 {
+    let power = 0.3;
+    match o {
+        OwningForce::RED => ImVec4::new(1.0, power, power, 1.0),
+        OwningForce::GREEN => ImVec4::new(power, 1.0, power, 1.0),
+        OwningForce::BLUE => ImVec4::new(power, power, 1.0, 1.0),
+        OwningForce::NEUTRAL => ImVec4::new(1.0, 1.0, 1.0, 1.0),
+    }
+}
+
+fn calc_map_dimensions(objective_definitions: &Vec<&ObjectiveDefinition>) -> MapDimensions {
+    let all_coords: Vec<[f32; 3]> = objective_definitions
+        .iter()
+        .map(|c| c.coord)
+        .filter(|c| c.is_some())
+        .map(|c| c.unwrap())
+        .collect();
+
+    let min_x = all_coords
+        .iter()
+        .map(|c| c[0])
+        .reduce(f32::min)
+        .unwrap_or(0.0);
+    let max_x = all_coords.iter()
+        .map(|c| c[0])
+        .reduce(f32::max).unwrap_or(0.0);
+    let min_y = all_coords
+        .iter()
+        .map(|c| c[1])
+        .reduce(f32::min).unwrap_or(0.0);
+    let max_y = all_coords.iter()
+        .map(|c| c[1])
+        .reduce(f32::max).unwrap_or(0.0);
+
+    let w = max_x - min_x;
+    let h = max_y - min_y;
+
+    MapDimensions {
+        min_x,
+        max_x,
+        min_y,
+        max_y,
+        w,
+        h,
+    }
+}
+
+struct MapDimensions {
+    min_x: f32,
+    max_x: f32,
+    min_y: f32,
+    max_y: f32,
+    w: f32,
+    h: f32,
 }

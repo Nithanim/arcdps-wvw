bc139caf5575cf93063ba1c6dfca3faf8fafc144
Nithanim <git@nithanim.me>
2023-12-17 00:03:17 +0100

    Hud refactor

--- lib/src/world3d/hud.rs
+++ lib/src/world3d/hud.rs
@@ -1,23 +1,29 @@
 use std::ops::Div;
 use imgui_sys::{igGetColorU32Vec4, igGetForegroundDrawList, ImDrawList_PathLineTo, ImDrawList_PathStroke, ImVec2, ImVec4};
-use mumblelink_reader::mumble_link::MumbleLinkData;
+use mumblelink_reader::mumble_link::{MumbleLinkData, MumbleLinkDataReader};
 use nalgebra::{Const, OMatrix, Point2, Point3, U4, Vector4};
+use crate::mumble::GuildwarsContext;
 use crate::settings::Settings;
 use crate::world3d::helpers;
 use crate::world3d::screen::get_screen_size;
 
 pub fn render_hud(settings: &Settings, ml: &MumbleLinkData) {
+    if get_current_map_id(ml) != 0 {
+        render_hud_(settings, ml);
+    }
+}
+pub fn render_hud_(settings: &Settings, ml: &MumbleLinkData) {
     let screen_size = get_screen_size();
     let view_projection = helpers::get_view_projection_matrix(&ml);
 
     if settings.show_objectives_overlay {
-        let h = H {
+        let gu = GraphicsUniform {
             screen_size,
             view_projection,
         };
 
         let target = Point3::new(-62.843487, 23.980408, 229.41426);
-        h.render(target, |imgui_coords| unsafe {
+        gu.render(target, |imgui_coords| unsafe {
             let draw_list = igGetForegroundDrawList();
             ImDrawList_PathLineTo(draw_list, ImVec2::new(imgui_coords.x - 100.0, imgui_coords.y - 100.0));
             ImDrawList_PathLineTo(draw_list, ImVec2::new(imgui_coords.x + 100.0, imgui_coords.y - 100.0));
@@ -28,12 +34,17 @@ pub fn render_hud(settings: &Settings, ml: &MumbleLinkData) {
     }
 }
 
-struct H {
+fn get_current_map_id(ml: &MumbleLinkData) -> u32 {
+    let gw2context = ml.read_context_into_struct::<GuildwarsContext>();
+    return gw2context.map_id;
+}
+
+struct GraphicsUniform {
     screen_size: ImVec2,
     view_projection: OMatrix<f32, U4, Const<4>>,
 }
 
-impl H {
+impl GraphicsUniform {
     fn render<F>(&self, world_coords: Point3<f32>, render_fn: F)
         where
             F: FnOnce(Point2<f32>) {
--- lib/src/world3d/pos_renderer.rs
+++ lib/src/world3d/pos_renderer.rs
@@ -1,9 +1,8 @@
 use c_str_macro::c_str;
 use imgui_sys::*;
-use mumblelink_reader::mumble_link::{MumbleLinkData, MumbleLinkReader};
+use mumblelink_reader::mumble_link::{MumbleLinkReader};
 use crate::world3d::screen::get_screen_size;
 use crate::{MUMBLE_LINK, SETTINGS};
-use crate::settings::Settings;
 use crate::world3d::hud::render_hud;
 
 const WINDOW_FLAGS: ImGuiWindowFlags = (ImGuiWindowFlags_NoBackground
@@ -21,16 +20,12 @@ pub unsafe fn render() {
     if handler.is_some() {
         let linked_memory = handler.unwrap().read().unwrap();
 
-        do_magic(&SETTINGS, &linked_memory);
+        if linked_memory.name == "Guild Wars 2" {
+            render_hud(&SETTINGS, &linked_memory)
+        }
     }
 
     //up is (0, 1, 0)
 
     igEnd();
-}
-
-unsafe fn do_magic(settings: &Settings, ml: &MumbleLinkData) {
-    //let gw2context = ml.read_context_into_struct::<GuildwarsContext>();
-
-    render_hud(settings, ml)
 }
